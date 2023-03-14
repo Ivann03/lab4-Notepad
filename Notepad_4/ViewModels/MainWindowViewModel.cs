b784d1a0c10fcdc7e1258f2a3f04359dfac8c224
Ivann03 <sentiv@DESKTOP-1DF5IGF.localdomain>
2023-03-14 15:43:49 +0700

    Изменение файла

--- Notepad_4/ViewModels/MainWindowViewModel.cs
+++ Notepad_4/ViewModels/MainWindowViewModel.cs
@@ -12,21 +12,18 @@ using System.IO;
 using System.Text;
 using static System.Net.WebRequestMethods;
 
+
 namespace Notepad_4.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-
         private ObservableCollection<Notepad> NotepadColl;
-
         public ObservableCollection<Notepad> NotepadList {  get => NotepadColl;  set => this.RaiseAndSetIfChanged(ref NotepadColl, value); }
 
-        private int curInd;
 
-        
 
         private bool visibleNote;
-
+        private int curInd;
         bool visibilityNote { get => visibleNote; set => this.RaiseAndSetIfChanged(ref visibleNote, value); }
 
 
@@ -36,37 +33,25 @@ namespace Notepad_4.ViewModels
 
 
         private string path = Directory.GetCurrentDirectory();
+        private string TextInBox, TextInFolder;
 
         private bool visible_open, visible_save;
 
-        private string TextInBox, TextInFolder;
 
         private string SaveButtonText = string.Empty;
 
         public MainWindowViewModel() 
         {
-            visibleNote = true;
             visibleExp = false;
-            visible_open = false;
+            visibleNote = true;
             visible_save = false;
-            TextInBox = string.Empty;
+            visible_open = false;
             TextInFolder = string.Empty;
+            TextInBox = string.Empty;
             SaveButtonText = "Открыть";
 
-
             NotepadColl = new ObservableCollection<Notepad>();
-
             GetPath(path);
-        }
-
-        public string TextDop
-        {
-            get => TextInFolder;
-            set
-            {
-                this.RaiseAndSetIfChanged(ref TextInFolder, value);
-                if (TextInFolder != "") ButtonText = "Сохранить";
-            }
         }
 
         public string ButtonText
@@ -77,15 +62,16 @@ namespace Notepad_4.ViewModels
                 this.RaiseAndSetIfChanged(ref SaveButtonText, value);
             }
         }
-
-        public string Text
+        public string TextDop
         {
-            get => TextInBox;
+            get => TextInFolder;
             set
             {
-                this.RaiseAndSetIfChanged(ref TextInBox, value);
+                this.RaiseAndSetIfChanged(ref TextInFolder, value);
+                if (TextInFolder != "") ButtonText = "Сохранить";
             }
         }
+
 
 
         public int CurrentIndexProp
@@ -116,15 +102,15 @@ namespace Notepad_4.ViewModels
             }
         }
 
-
-        public bool VisibleOpen
+        public string Text
         {
-            get => visible_open;
+            get => TextInBox;
             set
             {
-                this.RaiseAndSetIfChanged(ref visible_open, value);
+                this.RaiseAndSetIfChanged(ref TextInBox, value);
             }
         }
+
 
         public bool VisibleSave
         {
@@ -134,42 +120,42 @@ namespace Notepad_4.ViewModels
                 this.RaiseAndSetIfChanged(ref visible_save, value);
             }
         }
-
+        public bool VisibleOpen
+        {
+            get => visible_open;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref visible_open, value);
+            }
+        }
         public void returnBack()
         {
             TextInFolder = string.Empty;
-            visibilityNote = true;
-            visibilityExp = false;
             VisibleOpen = false;
             VisibleSave = false;
+            visibilityNote = true;
+            visibilityExp = false;
         }
 
 
         public void openExpl()
         {
             TextDop = string.Empty;
-            visibilityExp = true;
-            visibilityNote = false;
             VisibleOpen = true;
             VisibleSave = false;
+            visibilityExp = true;
+            visibilityNote = false;
         }
 
         public void saveExpl()
         {
             TextDop = "";
-            CurrentIndexProp = 0;
             visibilityExp = true;
-            visibilityNote = false;
-            VisibleOpen = false;
             VisibleSave = true;
+            visibilityNote = false;
+            CurrentIndexProp = 0;
+            VisibleOpen = false;
         }
-
-        public void ButtonClick()
-        {
-            if (VisibleOpen == true) ButtonOpen();
-            else if (VisibleSave == true) ButtonSave();
-        }
-
 
         public void ButtonOpen()
         {
@@ -198,7 +184,22 @@ namespace Notepad_4.ViewModels
                 returnBack();
             }
         }
+        public void ButtonClick()
+        {
+            if (VisibleOpen == true) ButtonOpen();
+            else if (VisibleSave == true) ButtonSave();
+        }
 
+        public void FileLoad(string ppath)
+        {
+            string new_text = String.Empty;
+            StreamReader sr = new StreamReader(NotepadColl[curInd].SourceName);
+            while (sr.EndOfStream != true)
+            {
+                new_text += sr.ReadLine() + "\n";
+            }
+            Text = new_text;
+        }
         public void ButtonSave()
         {
             if (NotepadColl[CurrentIndexProp] is Direct && TextDop == "")
@@ -232,37 +233,6 @@ namespace Notepad_4.ViewModels
         }
 
 
-        public void FileLoad(string ppath)
-        {
-            string new_text = String.Empty;
-            StreamReader sr = new StreamReader(NotepadColl[curInd].SourceName);
-            while (sr.EndOfStream != true)
-            {
-                new_text += sr.ReadLine() + "\n";
-            }
-            Text = new_text;
-        }
-
-        public async void FileSave(string ppath, int flag)
-        {
-            if (flag == 0)
-            {
-                using (StreamWriter writer = new StreamWriter(ppath))
-                {
-                    writer.Write(Text);
-                }
-            }
-            else
-            {
-                using (FileStream fstream = new FileStream(ppath, FileMode.OpenOrCreate))
-                {
-                    byte[] buffer = Encoding.Default.GetBytes(Text);
-                    await fstream.WriteAsync(buffer, 0, buffer.Length);
-                }
-            }
-        }
-
-
         public void GetPath(string warPath) 
         {
             NotepadColl.Clear();
@@ -289,7 +259,24 @@ namespace Notepad_4.ViewModels
             CurrentIndexProp = 0;
         }
 
-
+        public async void FileSave(string ppath, int flag)
+        {
+            if (flag == 0)
+            {
+                using (StreamWriter writer = new StreamWriter(ppath))
+                {
+                    writer.Write(Text);
+                }
+            }
+            else
+            {
+                using (FileStream fstream = new FileStream(ppath, FileMode.OpenOrCreate))
+                {
+                    byte[] buffer = Encoding.Default.GetBytes(Text);
+                    await fstream.WriteAsync(buffer, 0, buffer.Length);
+                }
+            }
+        }
         public void DoubleTap()
         {
             if (VisibleOpen == true) ButtonOpen();
